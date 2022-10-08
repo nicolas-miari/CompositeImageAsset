c04aaa7c5a4940c973cf2f2558cf07ad391ca982
Nicolás Miari <nicolasmiari@gmail.com>
2022-10-08 15:05:35 +0900

    Initial Commit

---
+++ Sources/CompositeImageAsset/CompositeImageAsset.swift
@@ -0,0 +1,6 @@
+public struct CompositeImageAsset {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CompositeImageAssetTests/CompositeImageAssetTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CompositeImageAsset
+
+final class CompositeImageAssetTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CompositeImageAsset().text, "Hello, World!")
+    }
+}
