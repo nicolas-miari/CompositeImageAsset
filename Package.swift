221acabcf5a323c5e3e48ae328d853923a8404c5
Nicolás Miari <nicolasmiari@gmail.com>
2022-10-08 15:24:01 +0900

    First working version

--- Package.swift
+++ Package.swift
@@ -4,25 +4,27 @@
 import PackageDescription
 
 let package = Package(
-    name: "CompositeImageAsset",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "CompositeImageAsset",
-            targets: ["CompositeImageAsset"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "CompositeImageAsset",
-            dependencies: []),
-        .testTarget(
-            name: "CompositeImageAssetTests",
-            dependencies: ["CompositeImageAsset"]),
-    ]
+  name: "CompositeImageAsset",
+  platforms: [
+    .iOS(.v15),
+    .macOS(.v11)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "CompositeImageAsset",
+      targets: ["CompositeImageAsset"]),
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    .package(url: "https://github.com/nicolas-miari/Asset.git", from: "1.0.2"),
+  ],
+  targets: [
+    .target(
+      name: "CompositeImageAsset",
+      dependencies: [
+        .product(name: "Asset", package: "Asset"),
+      ]),
+    // This package defines protocol only, and not implementations, so there are no tests.
+  ]
 )
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # CompositeImageAsset
 
-A description of this package.
+Defines a refinement of the `CompositeAsset` protocol for composite assets that are made up from
+multiple images, as well a refinement of the `CompositeAssetOptions` protocol used as the
+associated type.
